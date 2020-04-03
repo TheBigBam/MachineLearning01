a2b97bb8f5b196f4cc1890e444981a0a78dacb61
TheBigBam <43045929+TheBigBam@users.noreply.github.com>
2020-04-02 19:06:12 -0500

    Inicial

---
+++ Machine01/src/main/java/main.java
@@ -0,0 +1,20 @@
+import com.ibm.cloud.sdk.core.security.IamAuthenticator;
+import com.ibm.watson.visual_recognition.v3.VisualRecognition;
+import com.ibm.watson.visual_recognition.v3.model.ClassifiedImages;
+import com.ibm.watson.visual_recognition.v3.model.ClassifyOptions;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class main
+{
+
+
+
+    public static void main(String[] args) {
+        MachineGUI ex = new MachineGUI();
+        ex.setVisible(true);
+    }
+
+
+}
